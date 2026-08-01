aa935ee7020982d4a28160740c8e87ced23a4496
Shih Ching Fu <shih.ching.fu@gmail.com>
2026-08-01 17:54:17 +0800

    New function for validated posterior predictive draws.

--- mogp-utils.R
+++ mogp-utils.R
@@ -181,11 +181,17 @@ simulate_mogp <- function(D, Q, ns = 100, noise_sigma = 0.25, masked_pct = 0.2, 
   return(output_df)
 }
 
-mogp_postpred <- function(n_samples, x, y, y_se, d, D,
-              ns, # no. observations by band
-              x_star, d_star, ns_star,
-              w, Sigma, mu, theta, phi,
-              epsilon = 1e-9) {
+# Generate a single variate
+postpred_draw <- function(
+    x, y, y_se, d,
+    D,
+    ns, # no. observations by band
+    x_star,
+    d_star,
+    ns_star,
+    w, Sigma, mu, theta, phi,
+    seed = NULL,
+    epsilon = 1e-9) {
 
   N <- length(x);
   N_star <- length(x_star);
@@ -203,12 +209,122 @@ mogp_postpred <- function(n_samples, x, y, y_se, d, D,
   fstar_mu <- t(K_star) %*% solve(K_xx) %*% y
   fstar_Sigma <- K_starstar - t(K_star) %*% solve(K_xx) %*% K_star
 
-  #diag(fstar_Sigma) <- diag(fstar_Sigma) + epsilon
+  diag(fstar_Sigma) <- diag(fstar_Sigma) + epsilon
 
+  set.seed(seed)
   f_star <- rmvnorm(
-    n = n_samples,
+    n = 1,
     mean = fstar_mu,
     sigma = fstar_Sigma)
 
-  return (f_star)
+  result_df <- data.frame(
+    d_star = factor(d_star),
+    x_star,
+    f_star = t(f_star)
+  )
+
+  return(result_df)
+}
+
+postpred_draws <- function(n_draws = 1,
+                           x,
+                           y,
+                           y_se,
+                           d,
+                           D,
+                           ns, # no. observations by band
+                           x_star,
+                           d_star,
+                           ns_star,
+                           w, Sigma, mu, theta, phi,
+                           seed = NULL,
+                           epsilon = 1e-9) {
+
+  l <- vector("list", n_draws)
+
+  for (i in 1:n_draws) {
+
+    one_draw_df <- postpred_draw(
+      x = x, y = y, y_se = y_se,
+      d = d, D = D, ns = ns, x_star = x_star,
+      d_star = d_star, ns_star = ns_star,
+      w = w, Sigma = Sigma, mu = mu, theta = theta,
+      phi = phi, seed = seed, epsilon = epsilon)
+
+    l[[i]] <- one_draw_df
+  }
+
+  return( bind_rows(l, .id = ".draw"))
+}
+
+postpred_valid_draws <- function(
+    x,
+    y,
+    y_se,
+    d,
+    D,
+    ns,
+    x_star,
+    d_star,
+    ns_star,
+    ws,
+    Sigmas,
+    mus,
+    thetas,
+    phis,
+    seed = NULL,
+    epsilon = 1e-9) {
+
+  n_draws <- NROW(ws)
+  pp_list <- vector("list", n_draws)
+  valid_pps <- rep(FALSE, n_draws)
+
+  for (r in 1:n_draws) {
+    if (r %% 10 == 0)
+      cat(r,"of", n_draws,"\n")
+
+    this_w <- ws[r,]
+    this_Sigma <- Sigmas[r,]
+    this_mu <- mus[r,]
+    this_theta <- thetas[r,]
+    this_phi <- phis[r,]
+
+    tryCatch(
+      warning = function(cnd) {
+        cat("Warning:", r, conditionMessage(cnd), "\n")
+        pp_list[[r]] <- NA
+      },
+      error = function(cnd) {
+        cat("Error:", r, conditionMessage(cnd), "\n")
+        pp_list[[r]] <- NA
+      },
+      {
+        new_draw <- postpred_draw(
+          x = x,
+          y = y,
+          y_se = y_se,
+          d = d,
+          D = D,
+          ns = ns,
+          x_star = x_star,
+          d_star = d_star,
+          ns_star = ns_star,
+          this_w,
+          this_Sigma,
+          this_mu,
+          this_theta,
+          this_phi
+        ) |>
+          mutate(.draw = r)
+
+        pp_list[[r]] <- new_draw
+        valid_pps[r] <- TRUE
+      }
+    )
+  }
+
+  pp_df <- bind_rows(pp_list, .id = ".draw")
+
+  return( list(pp_df, valid_pps) )
+
 }
